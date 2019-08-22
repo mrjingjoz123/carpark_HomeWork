5e45f537dce136cfe961574eef90947b28c7c99b
mrjingjoz123 <59161111@go.buu.ac.th>
2019-08-22 22:12:58 +0700

    Puvis First times

---
+++ app/src/main/java/com/android/example/myhomework/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.android.example.myhomework
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.example.myhomework.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
